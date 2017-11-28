4ae6b3f7d9bd7bab64d9da23e5ad6db2c7444387
wesleyjiang <jiang.yuto@husky.neu.edu>
2017-11-27 21:17:00 -0500

    update Enemy

--- src/Enemy.h
+++ src/Enemy.h
@@ -7,14 +7,26 @@
 
 class Enemy { //maybe this will be abstract if there are multiple types of enemies
 public:
-	Enemy();//idk if we want this
-	Enemy(Vector pos);
+	Enemy(Vector start, Vector end);
 	void updatePosition();
+	void render(SDL_Renderer * renderer);
+	void updateEnemy();
 	void die();
 	bool collides(Vector v);
 	//do we need to check for colliding with platforms? If we make it so the player can drop them we should. If they stay on on-platform paths we don't have to
 private:
-	Vector position;
+	Vector start;
+	Vector end;
+	Vector posn;
+	Vector velocity;
+	Vector acceleration;
+	bool direction;
+
+	bool reachEnd();
+	void move();
+	void flip();
+	void updateVelocity();
+	
 };
 
 #endif
