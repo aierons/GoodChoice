8ec549a4586227743c38714ce1a56f37df4c0c65
wesleyjiang <jiang.yuto@husky.neu.edu>
2017-11-27 21:18:29 -0500

    Update enemy

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,13 +1,66 @@
 #include "Enemy.h"
 #include "Vector.hpp"
 
-Enemy::Enemy() { 
-	position = Vector(0, 0);
+Enemy::Enemy(Vector s, Vector e) {
+	start = s;
+	end = e;
+	direction = true;
+	posn = Vector(s);
+	velocity = Vector(5, 0);
+	acceleration = Vector(0, 0);
 }
-	//idk if we want this
-Enemy::Enemy(Vector pos) {
-	position = pos;
-}
-void Enemy::updatePosition() {}
 void Enemy::die() {}
 bool Enemy::collides(Vector v) { return false; }//dummy for now
+
+void g::updatePosition() {
+	if (this->direction) {
+		posn += velocity;
+	}
+	else {
+		posn -= velocity;
+	}
+	cout << posn.getX() << endl;
+}
+
+void g::move() {
+	if (this->direction) {
+		velocity[0] = 5; // Moves Right
+	}
+	else {
+		velocity[0] = -5; //Moves Left
+	}
+}
+
+bool g::reachEnd() {
+	return posn.getX() < start.getX()
+		&& posn.getX() > end.getX();
+}
+
+void g::updateEnemy() {
+	if (reachEnd()) {
+		flip();
+	}
+	updatePosition();
+}
+
+void g::updateVelocity() {
+	velocity += acceleration;
+}
+
+void g::flip() {
+	this->direction = !this->direction;
+}
+
+void g::render(SDL_Renderer * renderer) {
+	SDL_Texture * platformTex;
+	SDL_Rect destRect;
+
+	platformTex = TextureManager::loadTexture("res/enemy.png", renderer);
+	destRect.w = 23;
+	destRect.h = 23;
+
+	destRect.x = posn.getX();
+	destRect.y = 600 - posn.getY();
+
+	SDL_RenderCopy(renderer, platformTex, NULL, &destRect);
+}
