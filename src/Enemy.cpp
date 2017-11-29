1c90d9e964eb9f4a73867d1ffdf524dea74da785
wesleyjiang <jiang.yuto@husky.neu.edu>
2017-11-29 17:40:29 -0500

    Fixed enemy bug

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -6,86 +6,82 @@
 Enemy::Enemy(Vector s, Vector e) {
 	start = s;
 	end = e;
-	direction = true;
-	posn = s;
-	velocity = Vector(5, 0);
+	direction = RIGHT;
+	position = s;
+	velocity = Vector(1, 0);
 	acceleration = Vector(0, 0);
 }
+void Enemy::jump() {
+    velocity[1] += 1;
+    acceleration.setY(1.2);
+}
+void Enemy::updatePosition() {
+    position += velocity;
+}
+void Enemy::updateVelocity() {
+    velocity += acceleration;
+}
+void Enemy::updateAcceleration() {
+    if (isFalling) {
+        acceleration[1] -= .1;
+    } else {
+        acceleration[1] = 0;
+    }
+}
+
+
+void Enemy::updateDirection() {
+    if (position.getX() <= start.getX() && direction == LEFT) {
+        direction = RIGHT;
+    }
+    else if (position.getX() >= end.getX() && direction == RIGHT) {
+        direction = LEFT;
+    }
+}
+
+void Enemy::update(vector<Platform> platforms) {
+    if (direction == RIGHT) {
+        velocity[0] = 2;
+    } else {
+        velocity[0] = -2;
+    }
+    updatePosition();
+    updateDirection();
+}
+
 void Enemy::die() {
   delete this;
 }
-
 bool Enemy::collides(Vector v) {
-	vector<Vector> hitBox = this->getHitBox();
-	return v.getX() > hitBox.at(0).getX() && v.getX() > hitBox.at(1).getX()
-			&& v.getX() < hitBox.at(2).getX() && v.getX() < hitBox.at(3).getX()
-			&& v.getY() < hitBox.at(0).getY() && v.getY() < hitBox.at(1).getY()
-			&& v.getY() > hitBox.at(2).getY() && v.getY() < hitBox.at(3).getY();
-}
-
-
-void Enemy::updatePosition() {
-	if (this->direction) {
-		posn += velocity;
-	} else {
-		posn -= velocity;
-	}
-
-	cout << posn << endl;
-}
-
-void Enemy::move() {
-	if (this->direction) {
-		velocity[0] = 5; // Moves Right
-	}
-	else {
-		velocity[0] = -5; //Moves Left
-	}
-}
-
-bool Enemy::reachEnd() {
-	return posn.getX() < start.getX()
-		&& posn.getX() > end.getX();
-}
-
-void Enemy::updateEnemy() {
-	if (reachEnd()) {
-		flip();
-	}
-	updatePosition();
-}
-
-void Enemy::updateVelocity() {
-	velocity += acceleration;
-}
-
-void Enemy::flip() {
-	this->direction = !this->direction;
+    vector<Vector> hitBox = this->getHitBox();
+    return v.getX() > hitBox.at(0).getX() && v.getX() > hitBox.at(1).getX()
+            && v.getX() < hitBox.at(2).getX() && v.getX() < hitBox.at(3).getX()
+            && v.getY() < hitBox.at(0).getY() && v.getY() < hitBox.at(1).getY()
+            && v.getY() > hitBox.at(2).getY() && v.getY() < hitBox.at(3).getY();
 }
 
 vector<Vector> Enemy::getHitBox(){
-	int enemySize = 23;
-	vector<Vector> points;
-	//Top Left
-	points.push_back(Vector(this->posn.getX() - enemySize / 2, this->posn.getY() - enemySize / 2));
-	//Bottom Left .. etc
-	points.push_back(Vector(this->posn.getX() - enemySize / 2, this->posn.getY() + enemySize / 2));
-	points.push_back(Vector(this->posn.getX() + enemySize / 2, this->posn.getY() - enemySize / 2));
-	points.push_back(Vector(this->posn.getX() - enemySize / 2, this->posn.getY() + enemySize / 2));
-
+    int enemySize = 23;
+    vector<Vector> points;
+    //Top Left
+    points.push_back(Vector(this->position.getX() - enemySize / 2, this->position.getY() - enemySize / 2));
+    //Bottom Left .. etc
+    points.push_back(Vector(this->position.getX() - enemySize / 2, this->position.getY() + enemySize / 2));
+    points.push_back(Vector(this->position.getX() + enemySize / 2, this->position.getY() - enemySize / 2));
+    points.push_back(Vector(this->position.getX() - enemySize / 2, this->position.getY() + enemySize / 2));
   return points;
 }
 
 void Enemy::render(SDL_Renderer * renderer) {
-	SDL_Texture * platformTex;
+	SDL_Texture * enemyTex;
 	SDL_Rect destRect;
 
-	platformTex = TextureManager::loadTexture("res/enemy.png", renderer);
+	enemyTex = TextureManager::loadTexture("res/enemy.png", renderer);
 	destRect.w = 23;
 	destRect.h = 23;
 
-	destRect.x = posn.getX();
-	destRect.y = 600 - posn.getY();
+	destRect.x = position.getX();
+	destRect.y = 600 - position.getY();
 
-	SDL_RenderCopy(renderer, platformTex, NULL, &destRect);
+	SDL_RenderCopy(renderer, enemyTex, NULL, &destRect);
 }
