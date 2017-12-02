ce5a381582715b4aa992a9ebe549f6610b66204c
wesleyjiang <jiang.yuto@husky.neu.edu>
2017-12-02 10:26:39 -0500

    update

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -8,14 +8,7 @@ Enemy::Enemy(Vector s, Vector e) {
     velocity = Vector(1, 0);
     acceleration = Vector(0, 0);
 }
-Enemy::Enemy() {}
-void Enemy::jump() {
-    velocity[1] += 1;
-    acceleration.setY(1.2);
-}
-void Enemy::updatePosition() {
-    position += velocity;
-}
+
 void Enemy::updateVelocity() {
     velocity += acceleration;
 }
@@ -25,6 +18,32 @@ void Enemy::updateAcceleration() {
     //} else {
     //    acceleration[1] = 0;
     //}
+}
+
+void Enemy::update(Player player) {
+    if (direction) {
+        velocity[0] = 2;
+    } else {
+        velocity[0] = -2;
+    }
+    updatePosition(player);
+    updateDirection();
+}
+void Enemy::updatePosition(Player player) {
+    position += velocity;
+}
+void Enemy::render(SDL_Renderer * renderer) {
+    SDL_Texture * enemyTex;
+    SDL_Rect destRect;
+    
+    enemyTex = TextureManager::loadTexture("/Users/wesleyjiang/Desktop/Testing/res/enemy.png", renderer);
+    destRect.w = 23;
+    destRect.h = 23;
+    
+    destRect.x = position.getX();
+    destRect.y = 600 - position.getY();
+    
+    SDL_RenderCopy(renderer, enemyTex, NULL, &destRect);
 }
 
 
@@ -37,50 +56,12 @@ void Enemy::updateDirection() {
     }
 }
 
-void Enemy::update(vector<Platform> platforms) {
-    if (direction) {
-        velocity[0] = 2;
-    } else {
-        velocity[0] = -2;
+bool Enemy::collides(Vector point) {
+    if (point.getX() <= position.getX() + 15 && point.getX() >= position.getX() - 15
+        && point.getY() <= position.getY() + 15 && point.getY() >= position.getY() - 15) {
+        return true;
     }
-    updatePosition();
-    updateDirection();
-}
-
-void Enemy::die() {
-    
-}
-bool Enemy::collides(Vector v) {
-    vector<Vector> hitBox = this->getHitBox();
-		return v.getX() > hitBox.at(0).getX() && v.getX() > hitBox.at(1).getX()
-           && v.getX() < hitBox.at(2).getX() && v.getX() < hitBox.at(3).getX()
-           && v.getY() < hitBox.at(0).getY() && v.getY() < hitBox.at(1).getY()
-           && v.getY() > hitBox.at(2).getY() && v.getY() < hitBox.at(3).getY();
-	//return v == this->position;
-}
-
-vector<Vector> Enemy::getHitBox(){
-    int enemySize = 23;
-    vector<Vector> points;
-    //Top Left
-    points.push_back(Vector(this->position.getX() - enemySize / 2, this->position.getY() - enemySize / 2));
-    //Bottom Left .. etc
-    points.push_back(Vector(this->position.getX() - enemySize / 2, this->position.getY() + enemySize / 2));
-    points.push_back(Vector(this->position.getX() + enemySize / 2, this->position.getY() - enemySize / 2));
-    points.push_back(Vector(this->position.getX() - enemySize / 2, this->position.getY() + enemySize / 2));
-    return points;
-}
-
-void Enemy::render(SDL_Renderer * renderer) {
-    SDL_Texture * enemyTex;
-    SDL_Rect destRect;
-
-    enemyTex = TextureManager::loadTexture("res/enemy.png", renderer);
-    destRect.w = 23;
-    destRect.h = 23;
-
-    destRect.x = position.getX();
-    destRect.y = 600 - position.getY();
-
-    SDL_RenderCopy(renderer, enemyTex, NULL, &destRect);
+    else {
+        return false;
+    }
 }
