10727c39888efdf35e179203baba727e1cabbdbc
wesleyjiang <jiang.yuto@husky.neu.edu>
2017-12-04 17:50:11 -0500

    g

--- src/FlyingEnemy.cpp
+++ src/FlyingEnemy.cpp
@@ -1,21 +1,9 @@
-//
-//  NormalEnemy.cpp
-//  testing
-//
-//  Created by Wesley Jiang on 12/1/17.
-//  Copyright © 2017 Wesley Jiang. All rights reserved.
-//
-
 #include "FlyingEnemy.hpp"
 
 FlyingEnemy::FlyingEnemy(Vector s, Vector e) : Enemy(s, e) {}
 
 void FlyingEnemy::update(Player player) {
-    if (direction) {
-        velocity[0] = 2;
-    } else {
-        velocity[0] = -2;
-    }
+
     updatePosition(player);
     updateDirection();
 }
@@ -25,13 +13,15 @@ void FlyingEnemy::updatePosition(Player player) {
     int y = (int) this->position.getY();
     int x2 = (int) player.position.getX();
     int y2 = (int) player.position.getY();
-    Vector slope = Vector((x2 - x) / 25, (y2 - y) / 25);
+    Vector slope = Vector((x2 - x) / 15, (y2 - y) / 15);
+
 	if (player.isIdle()) {
 		// still gotta move if it is idle
 	}
 	else {
 		if (this->position.getX() > player.position.getX()) {
 			this->position += slope;
+            direction = RIGHT;
 		}
 		else {
 			this->position -= -1 * slope;
@@ -43,7 +33,12 @@ void FlyingEnemy::render(SDL_Renderer * renderer) {
     SDL_Texture * enemyTex;
     SDL_Rect destRect;
     
-    enemyTex = TextureManager::loadTexture("res/flying.png", renderer);
+    if (direction == RIGHT) {
+        enemyTex = TextureManager::loadTexture("res/flyingRight.png", renderer);
+    }
+    else {
+        enemyTex = TextureManager::loadTexture("res/flyingLeft.png", renderer);
+    }
     destRect.w = 40;
     destRect.h = 40;
     
@@ -53,6 +48,5 @@ void FlyingEnemy::render(SDL_Renderer * renderer) {
     SDL_RenderCopy(renderer, enemyTex, NULL, &destRect);
 	SDL_DestroyTexture(enemyTex);
 }
-
 
 
