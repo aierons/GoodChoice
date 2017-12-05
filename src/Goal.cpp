e35e74cff2e6aa2fad22d9167933127a7c60a05c
risleychris <risley.c@husky.neu.edu>
2017-12-04 22:41:17 -0500

    Update Goal.cpp

--- src/Goal.cpp
+++ src/Goal.cpp
@@ -9,16 +9,20 @@ bool Goal::collides(Vector v) {
 		&& (v.getY() - position.getY() < 20 && v.getY() - position.getY() > -20);
 }
 void Goal::render(SDL_Renderer * renderer) {
-	SDL_Texture * tex;
-	SDL_Rect destRect;
+    SDL_Rect destRect;
+    
+    if (texture == NULL){
+        texture = TextureManager::loadTexture("/res/Goal.png", renderer);
+    }
+    destRect.w = 30;
+    destRect.h = 30;
+    
+    destRect.x = position.getX();
+    destRect.y = (630 - destRect.h) - position.getY();
+    
+    SDL_RenderCopy(renderer, texture, NULL, &destRect);
+}
 
-	tex = TextureManager::loadTexture("res/Goal.png", renderer);
-	destRect.w = 30;
-	destRect.h = 30;
-
-	destRect.x = position.getX();
-	destRect.y = (630 - destRect.h) - position.getY();
-
-	SDL_RenderCopy(renderer, tex, NULL, &destRect);
-	SDL_DestroyTexture(tex);
+void Goal::deleteTexture(){
+    SDL_DestroyTexture(texture);
 }
