9aeb166e2af5f52dc31bcb29a4ac8f823621dd04
jai <ai.j@husky.neu.edu>
2017-12-05 22:56:10 -0500

    Added tests

---
+++ test/GoalTest.hpp
@@ -0,0 +1,15 @@
+#include <cxxtest/TestSuite.h>
+#include <iostream>
+#include "Goal.h"
+
+class GoalTest : public CxxTest::TestSuite
+{
+public:
+  //testing if the goal constructs and collides accordingly
+  void testGoalCollidesr(void) {
+    Vector *v1 = new Vector(40, 54);
+    Goal g1 = new Goal(v1);
+    Vector *v2 = new Vector(30, 40);
+    TS_ASSERT(g1.collides(v2));
+  }
+};
