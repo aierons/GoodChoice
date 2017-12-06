608c06794426fc06a6b58f3ac67bf3c5092f4ac5
jai <ai.j@husky.neu.edu>
2017-12-05 23:51:06 -0500

    Update BulletTest.hpp

--- test/BulletTest.hpp
+++ test/BulletTest.hpp
@@ -12,28 +12,27 @@ class BulletTest : public CxxTest::TestSuite
 public:
   //testing if the bullet constructs appropriately
   void testConstructor(void) {
-    Vector v1 = new Vector(50, 50);
-    Vector v2 = new Vector(100, 100);
-    Bullet bullet = new Bullet(v1, v2);
-    TS_ASSERT_EQUALS(bullet.position, new Vector(50, 50));
-    TS_ASSERT_EQUALS(bullet.velocity, new Vector(100, 100));
+    Vector *v1 = new Vector(50, 50);
+    Vector *v2 = new Vector(100, 100);
+    Bullet *bullet = new Bullet(v1, v2);
+    TS_ASSERT_EQUALS(bullet->getPostion(), new Vector(50, 50));
   }
 
   //testing if the bullet is alive when it's initially created
   void testIsAlive(void) {
-    Vector v1 = new Vector(50, 50);
-    Vector v2 = new Vector(100, 100);
-    Bullet bullet = new Bullet(v1, v2);
-    TS_ASSERT(bullet.isAlive());
+    Vector *v1 = new Vector(50, 50);
+    Vector *v2 = new Vector(100, 100);
+    Bullet *bullet = new Bullet(v1, v2);
+    TS_ASSERT(bullet->isAlive());
   }
 
   //testing if updatePosition updates the bullet's position properly and if the bullet is still alive
   void testUpdatePosition(void) {
-    Vector v1 = new Vector(50, 50);
-    Vector v2 = new Vector(100, 100);
-    Bullet bullet = new Bullet(v1, v2);
-    bullet.updatePosition();
-    TS_ASSERT_EQUALS(bullet.getPosition(), new Vector(150, 150));
-    TS_ASSERT(bullet.isAlive());
+    Vector *v1 = new Vector(50, 50);
+    Vector *v2 = new Vector(100, 100);
+    Bullet *bullet = new Bullet(v1, v2);
+    bullet->updatePosition();
+    TS_ASSERT_EQUALS(bullet->getPosition(), new Vector(150, 150));
+    TS_ASSERT(bullet->isAlive());
   }
 };
