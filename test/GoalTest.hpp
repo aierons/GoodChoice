6a76b398012acfffe9d96e26c6fabbc2db54d4c7
jai <ai.j@husky.neu.edu>
2017-12-05 23:45:46 -0500

    Update GoalTest.hpp

--- test/GoalTest.hpp
+++ test/GoalTest.hpp
@@ -7,7 +7,7 @@ class GoalTest : public CxxTest::TestSuite
 {
 public:
   //testing if the goal constructs and collides accordingly
-  void testGoalCollidesr(void) {
+  void testGoalCollides(void) {
     Vector *v1 = new Vector(40, 54);
     Goal g1 = new Goal(v1);
     Vector *v2 = new Vector(30, 40);
